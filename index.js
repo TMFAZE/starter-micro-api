59cc29796aeed00bd55f809090762c755de0499d
TMFAZE <43424261+TMFAZE@users.noreply.github.com>
2023-04-15 11:18:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+import discord
+from discord.ext import commands
+from discord.utils import get
+import youtube_dl
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command()
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+@bot.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+
+@bot.command()
+async def play(ctx, url):
+    song = os.path.isfile("song.mp3")
+    try:
+        if song:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+        return
+
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name=str(ctx.author.voice.channel))
+    await voiceChannel.connect()
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': 'song.mp3',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
+@bot.command()
+async def stop(ctx):
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    voice.stop()
+
+bot.run('YOUR BOT TOKEN HERE')
